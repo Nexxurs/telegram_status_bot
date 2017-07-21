0e09e72ad7721d91b52778b41144585bc696f70f
Nexxurs <p.uebermasser@gmx.at>
2017-07-21 18:21:05 +0200

    Minor Changes

--- bot.py
+++ bot.py
@@ -10,38 +10,60 @@ import os
 from threading import Thread
 from time import sleep
 
-def restartSoon():
-    sleep(1)
-    os.execv(sys.executable, ['python'] + sys.argv)
+filePath = ''
+
+def pull(args = None):
+	if filePath == '':
+		print("Cannot pull without Filepath!")
+	else:
+		bot.sendMessage(chat_id, "Pulling..")
+		call("cd ~/git/telegram_status_bot && git pull", shell=True)
+		bot.sendMessage(chat_id, "Finished!")
+	
+def restart(args = None):
+    def restartSoon():
+        sleep(1)
+        os.execv(sys.executable, ['python'] + sys.argv)
+    bot.sendMessage(chat_id, "Restarting... ")
+    t = Thread(target = restartSoon)
+    t.start()
+	
+	
+	
+functions = {'/pull' : pull, 
+    '/restart' : restart}
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id, msg['text'])
-
-    if(msg['text'] == "/pull"):
-        bot.sendMessage(chat_id, "Pulling..")
-        call("cd ~/git/telegram_status_bot && git pull", shell=True)
-        bot.sendMessage(chat_id, "Finished!")
-    elif(msg['text'] == "/restart"):
-        bot.sendMessage(chat_id, "Restarting... ")
-        t = Thread(target = restartSoon)
-        t.start()
+	
+    msgArgs = msg['text'].split(' ')
+	
+    if msgArgs[0] in functions:
+        functions[msg['text']](msgArgs)	
     else:
         bot.sendMessage(chat_id, "wat?")
 
 
 
-config = configparser.ConfigParser()
-config.read("/home/pi/git/telegram_status_bot/config.ini")
 
-token = config['Telegram']['Token']
-token = str(token)
 
-bot = telepot.Bot(token)
 
-MessageLoop(bot, handle).run_as_thread()
-print ('Listening ...')
+if __name__ == '__main__':
 
-# Keep the program running.
-while 1:
-    time.sleep(10)
+    filePath = os.path.dirname(os.path.realpath(__file__))
+
+    config = configparser.ConfigParser()
+    config.read(filePath+"/config.ini")
+
+    token = config['Telegram']['Token']
+    token = str(token)
+
+    bot = telepot.Bot(token)
+
+    MessageLoop(bot, handle).run_as_thread()
+    print ('Listening ...')
+
+    # Keep the program running.
+    while 1:
+        time.sleep(10)
