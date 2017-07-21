199a03201c7ad2cb4b1604a4176a996603510487
Paul Übermaßer <p.uebermasser@gmx.at>
2017-07-21 16:24:21 +0200

    wip

--- bot.py
+++ bot.py
@@ -1,1 +1,20 @@
-print("hello")
+import configparser
+import telepot
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if(msg['text'] == "pull"):
+        bot.sendMessage(chat_id, "Pulling.. "+chat_id)
+
+config = configparser.ConfigParser()
+config.read("/home/pi/git/telegram_status_bot/config.ini")
+
+token = config['Telegram']['Token']
+token = str(token)
+
+bot = telepot.Bot(token)
+
+
