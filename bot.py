49c5778a4cd98b030365134b852599814b721395
Nexxurs <p.uebermasser@gmx.at>
2017-07-23 16:15:01 +0200

    Cleanup, Bugfixes and Shell execution reworked

--- bot.py
+++ bot.py
@@ -1,81 +1,93 @@
 import configparser
 import telepot
 from telepot.loop import MessageLoop
-import time
 from time import sleep
 import helper
+from sys import exit
 
 _VERSION = 0.1
 
-filePath = ''
-admins = []
- 
+filePath = None
+admins = None
+bot = None
+
+
 def createHeader():
+    if bot is None:
+        raise ReferenceError("Cannot create Header without Bot Context!")
+
     me = bot.getMe()
     string = "\n"
-    string = string +"####################################################\n"
-    string = string +"   Name: "+me['first_name']+"\n"
-    string = string +"   Username: "+me['username']+"\n"
-    string = string +"   ID: "+str(me['id'])+"\n"
-    string = string +"   Branch: "+helper.getGitBranch()+"\n"
-    string = string +"   Version: "+str(_VERSION)+"\n"
-    string = string +"####################################################\n"
+    string = string + "####################################################\n"
+    string = string + "   Name: " + me['first_name'] + "\n"
+    string = string + "   Username: " + me['username'] + "\n"
+    string = string + "   ID: " + str(me['id']) + "\n"
+    string = string + "   Branch: " + str(helper.getGitBranch()) + "\n"
+    string = string + "   Version: " + str(_VERSION) + "\n"
+    string = string + "####################################################\n"
     return string
-    
-def aboutme(chat_id, args = None):
+
+
+def aboutme(chat_id, args=None):
+    if bot is None:
+        raise ReferenceError("Cannot create AboutMe without Bot Context!")
     bot.sendMessage(chat_id, createHeader())
-    
-    
-functions = {'/pull' : helper.pull, 
-    '/restart' : helper.restart,
-    '/aboutme' : aboutme}
-    
+
+
+functions = {'/pull': helper.pull,
+             '/restart': helper.restart,
+             '/aboutme': aboutme}
+
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id, msg['text'])
-    
-    #admins are strings and it seems to work sending messages to String IDs
+
+    # admins are strings and it seems to work sending messages to String IDs
     chat_id = str(chat_id)
-    
+
     if not chat_id in admins:
         print("Message from unknown User!")
         bot.sendMessage("I'm sorry, but my Daddy says im not allowed to speak to Strangers!")
         return
-        
-    msgArgs = msg['text'].split(' ')
-    
-    if msgArgs[0] in functions:
-        functions[msg['text']](chat_id, msgArgs) 
-    else:
-        bot.sendMessage(chat_id, "wat?")
 
+    msgArgs = msg['text'].split(' ')
+
+    if msgArgs[0] in functions:
+        functions[msgArgs[0]](chat_id, msgArgs)
+    else:
+        bot.sendMessage(chat_id, "Error 404: Function not found!")
 
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    config.read(helper.getFilePath()+"/config.ini")
+    config.read(helper.getFilePath() + "/config.ini")
 
     token = config['Telegram']['Token']
     token = str(token)
-    
+
     admins = config['Telegram']['Admins']
     admins = admins.split(',')
 
     bot = telepot.Bot(token)
-    
-    helper.setBot(bot)
-    
-    me = bot.getMe()
-    print(createHeader())
+
+    helper.bot = bot
+    helper.admins = admins
+
+    try:
+        print(createHeader())
+    except Exception as e:
+        print("Cannot start Bot, Connection Error!")
+        print(e)
+        exit()
 
     MessageLoop(bot, handle).run_as_thread()
-    print ('Listening ...')
-    
-    #Say Hello to our Admins!
+    print('Listening ...')
+
+    # Say Hello to our Admins!
     for admin in admins:
         bot.sendMessage(admin, "Hello, im Back!")
 
     # Keep the program running.
     while 1:
-        time.sleep(10)
+        sleep(10)
--- helper.py
+++ helper.py
@@ -1,43 +1,71 @@
-from subprocess import call
+import subprocess
 import sys
 import os
 from threading import Thread
 from pygit2 import Repository
+from time import sleep
 
 filePath = os.path.dirname(os.path.realpath(__file__))
 bot = None
+admins = []
 
-def setBot(tel_bot):
-    bot = tel_bot
-    
+
+def _execute(cmd):
+    process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                             shell=True)
+    out = process.stdout.decode('utf-8')
+    err = process.stderr.decode('utf-8')
+    return out, err
+
+
 def getFilePath():
     return filePath
-    
+
+
 def getGitBranch():
-    repo = Repository(filePath+"/.git")
-    return repo.head.shorthand
-    
-def pull(chat_id, args = None):
-    #Define Branch! When None, Default pull!
+    try:
+        repo = Repository(filePath + "/.git")
+        return repo.head.shorthand
+    except Exception as e:
+        return None
+
+
+def pull(chat_id, args=None):
+    # Define Branch! When None, Default pull!
     if filePath == '':
         print("Cannot pull without Filepath!")
+        return
+    if bot is None:
+        raise ReferenceError("Cannot use Function without Bot Context!")
+
+    if len(args) > 1:
+        # TODO Checkout new Branch!
+        print("New Branch: "+args[1])
+        out, err = _execute("cd " + filePath + " && git pull")
     else:
-        bot.sendMessage(chat_id, "Pulling..")
-        call("cd "+filePath+" && git pull", shell=True)
-        bot.sendMessage(chat_id, "Finished!")
-    
-def restart(chat_id, args = None):
-    def restartSoon():
+        out, err = _execute("cd " + filePath + " && git pull")
+
+    if len(err)>0:
+        result = "ERROR:\n"+err
+    else:
+        result = out
+    bot.sendMessage(chat_id, result+"\nTask finished!")
+
+
+def restart(chat_id, args=None):
+    if bot is None:
+        raise ReferenceError("Cannot use Function without Bot Context!")
+
+    def restart_soon():
         sleep(1)
         os.execv(sys.executable, ['python'] + sys.argv)
-    
+
     bot.sendMessage(chat_id, "Restarting... ")
-    
+
     for admin in admins:
         if admin != chat_id:
-            print("Debug: Admin ID: "+admin+", chat_id: "+chat_id)
+            print("Debug: Admin ID: " + admin + ", chat_id: " + chat_id)
             bot.sendMessage(admin, "Just fyi: Someone ordered me to restart!")
-    
-    t = Thread(target = restartSoon)
+
+    t = Thread(target=restart_soon)
     t.start()
-    
