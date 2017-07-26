79a16b74a1146e3b983b90e1be6804859a03f13b
Paul Übermaßer <p.uebermasser@gmx.at>
2017-07-26 22:27:31 +0200

    pihole module

---
+++ modules/pihole.py
@@ -0,0 +1,28 @@
+from modules.core_module import CoreModule
+from urllib import request, error
+import json
+
+class Module(CoreModule):
+
+    def __init__(self, bot, config):
+        super().__init__(bot, config)
+        try:
+            req = request.urlopen("http://localhost/admin/api.php")
+            self._enabled = True
+        except error.HTTPError | error.URLError:
+            self._enabled = False
+
+    def is_enabled(self):
+        return self._enabled
+
+    def get_chat_functions(self):
+        return {'/pihole', self.get_status}
+
+    def get_status(self, chat_id, args=None):
+        req = request.urlopen("http://localhost/admin/api.php")
+        txt = req.read().decode()
+        dic = json.loads(txt)
+        msg = 'Pi-Hole Stats of the Day:\n'
+        for key in dic.keys():
+            msg = msg + key + ': '+dic[key]+'\n'
+        self._bot.sendMessage(chat_id, msg)
